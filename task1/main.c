a0ba1a8e045476174c4d02f095e2a29f3ad9d0e7
forrisdahl <nathanif@stud.ntnu.no>
2025-08-21 19:11:28 +0200

    feat: added main.c example file

---
+++ task1/main.c
@@ -0,0 +1,13 @@
+
+#include <avr/io.h>
+#include <stdlib.h>
+
+int main(void) {
+    // Initialization code here
+    // Example: configure pins
+
+    while (1) {
+        // Main loop
+    }
+    return EXIT_FAILURE;
+}
